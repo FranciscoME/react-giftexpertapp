9e07f0903b356db24e3459dcd6edba0841132e90
Francisco Márquez E <fmarquezestrada@gmail.com>
2020-08-25 19:02:04 -0500

    Commit Build-Docs OK

---
+++ docs/precache-manifest.678ef5d79e0ac06f0b30c92cb15fcd3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9116727709ad1c937c0ecb4726ae610",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78a10de815f86288bc14",
+    "url": "/static/css/main.e1ab76df.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a10de815f86288bc14",
+    "url": "/static/js/main.2fe75516.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
